8bf962a6097f78b69240dc124374d523fbb2c299
Administrator <Administrator@MININT-L393FMB>
2020-10-29 16:21:09 -0700

    [K8S] Demo Application to test Kubernetes

---
+++ src/main/java/com/example/demo/K8sSpringbootDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sSpringbootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sSpringbootDemo1Application.class, args);
+	}
+
+}
